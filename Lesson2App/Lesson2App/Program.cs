52e6ce5f03a2aa6a4360791c49a79068f94537c4
rudite.pruse@gmail.com <rudite.pruse@gmail.com>
2021-09-07 22:04:21 +0300

    Lesson2test

---
+++ Lesson2App/Lesson2App/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lesson2App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Ievadi savu vecumu?");
+            string userAgeText = Console.ReadLine();
+
+
+
+        }
+    }
+}
