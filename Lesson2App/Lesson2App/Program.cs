458c7a8ee0d4c494527bde9779626c39f7922900
rudite.pruse@gmail.com <rudite.pruse@gmail.com>
2021-09-11 09:57:43 +0300

    updated

--- Lesson2App/Lesson2App/Program.cs
+++ Lesson2App/Lesson2App/Program.cs
@@ -34,30 +34,16 @@ namespace Lesson2App
             string maxNumber1 = Console.ReadLine();
             string maxNumber2 = Console.ReadLine();
 
+            int maxLargest = Math.Max(maxNumber1, maxNumber2);
+            
 
-            Console.WriteLine(maxNumber1, maxNumber2);
+            Console.WriteLine(maxLargest);
 
             Console.WriteLine("-------------------------------------------------------");
 
-            bool isEqual = 15 == 17;
-            Console.WriteLine(isEqual);
+            
 
-            bool isNotEqual = !isEqual;
-            Console.WriteLine(isNotEqual);
-
-            int isSmallerNumber1 = 15;
-            int isSmallerNumber2 = 17;
-            bool isSmaller = isSmallerNumber1 <= isSmallerNumber2;
-            Console.WriteLine(isSmaller);
-
-            bool iAmTrue = true;
-            bool iAmFalse = false;
-
-            bool result1 = iAmTrue && iAmFalse;
-            bool result2 = iAmTrue || iAmFalse;
-
-            Console.WriteLine(result1);
-            Console.WriteLine(result2);
+           
 
 
 
